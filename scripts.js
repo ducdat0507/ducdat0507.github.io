b9058b301bb1fd09824ff39a31e83c7a2177aaca
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2021-05-25 19:39:22 +0700

    Updated games list

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 var flavorTexts = [
     "( Made with love and Github Pages )",
     "( Made with love and some Javascript magic )",
-    "( Made with Car- wait, no, it was Github Pages )",
+    "( Made with Car- wait, no, it wasn't )",
     "( Made with love by ducdat0507 )",
+    "( This is a flavor text, oh and, made by ducdat0507 )",
+    "( Something... love... something... ducdat0507 )",
+    "( Look ma, I'm on the internet! -ducdat0507 )",
 ]
 
 function onLoad() {
     document.getElementById("mainDiv").classList.remove("intro")
-    setTimeout(() => hasSkipped = true, 5000)
+    setTimeout(() => { 
+        hasSkipped = true 
+        document.getElementById("mainDiv").style.pointerEvents = ''
+    }, 5000)
 
     document.getElementById("flavor").innerText = flavorTexts[Math.floor(Math.random() * flavorTexts.length)]
 }
@@ -17,6 +23,7 @@ function skipIntro() {
     if (!hasSkipped) {
         var d = document.getElementById("mainDiv")
         d.style.transition = '0.5s'
+        d.style.pointerEvents = ''
         d.classList.add("intro")
         setTimeout(() => d.classList.remove("intro"), 0)
         hasSkipped = true

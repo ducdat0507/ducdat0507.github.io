09be41239d495a7c6218611270ba6b167591a6fd
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-08-18 23:32:33 +0700

    touch tooltips

--- src/components/utils/Tooltip.svelte
+++ src/components/utils/Tooltip.svelte
@@ -11,36 +11,77 @@
 
     let tooltip: HTMLElement;
 
-    let isShowing = $state(false);
-
-    let left: number = $state(0);
-    let top: number = $state(0);
+    let isShowing = false;
 
     onMount(() => {
         let parent = tooltip.parentNode as HTMLElement | null;
         if (!parent) return;
 
-        parent.addEventListener("pointerenter", onTooltipSet);
-        parent.addEventListener("pointerleave", onTooltipUnset);
+        parent.addEventListener("pointerenter", onTooltipSetMouse);
+        parent.addEventListener("pointerleave", onTooltipUnsetMouse);
+        parent.addEventListener("pointerdown", onTooltipSetTouch);
         parent.addEventListener("focus", onTooltipSet);
         parent.addEventListener("blur", onTooltipUnset);
 
         return () => {
-            parent.removeEventListener("pointerenter", onTooltipSet);
-            parent.removeEventListener("pointerleave", onTooltipUnset);
+            parent.removeEventListener("pointerenter", onTooltipSetMouse);
+            parent.removeEventListener("pointerleave", onTooltipUnsetMouse);
+            parent.removeEventListener("pointerdown", onTooltipSetTouch);
             parent.removeEventListener("focus", onTooltipSet);
             parent.removeEventListener("blur", onTooltipUnset);
         }
     })
 
     function onTooltipSet() {
+        isShowing = true;
         let parent = tooltip.parentNode as HTMLElement | null;
         if (!parent) return;
         setTooltip(parent, children, containerProps);
     }
 
     function onTooltipUnset() {
+        isShowing = false;
         unsetTooltip();
+    }
+
+    function onTooltipSetMouse(ev: PointerEvent) {
+        if (ev.pointerType != "touch") {
+            onTooltipSet();
+        }
+    }
+
+    function onTooltipUnsetMouse(ev: PointerEvent) {
+        if (ev.pointerType != "touch") {
+            onTooltipUnset();
+        }
+    }
+
+    function cancelTouch(ev: PointerEvent) {
+        ev.preventDefault();
+    }
+
+    function onTooltipSetTouch(ev: PointerEvent) {
+        if (ev.pointerType == "touch") {
+            let parent = tooltip.parentNode as HTMLElement | null;
+            if (!parent) return;
+
+            if (isShowing) return;
+
+            ev.stopPropagation();
+            
+            onTooltipSet();
+
+            function bodyTouch(ev2: PointerEvent) {
+                onTooltipUnset();
+                document.body.removeEventListener("pointerdown", bodyTouch);
+                document.body.removeEventListener("pointermove", bodyTouch);
+                parent?.removeEventListener("click", cancelTouch);
+            }
+            document.body.addEventListener("pointerdown", bodyTouch);
+            document.body.addEventListener("pointermove", bodyTouch);
+            parent.addEventListener("click", cancelTouch);
+        }
+        else onTooltipUnset();
     }
 </script>
 

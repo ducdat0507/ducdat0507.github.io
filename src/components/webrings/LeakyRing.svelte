fa56ce27bf75b82d166fc6e4d0b1c7427adadf4e
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-09-04 21:17:27 +0700

    fix leaky ring 9

--- src/components/webrings/LeakyRing.svelte
+++ src/components/webrings/LeakyRing.svelte
@@ -90,7 +90,7 @@
 <style>
     .leaky-ring-holder {
         position: absolute;
-        border: none;
+        border-width: 0px;
         inset: 0;
         background: url(/index/res/images/tiling-bg.svg) repeat, linear-gradient(#5df, #59f);
         mask: linear-gradient(white, white) no-repeat bottom / 100% calc(var(--level, 0) * 100%);
